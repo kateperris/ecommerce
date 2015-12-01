790c175e8935cdd4edcf5d0cdb95416d64d2c376
Kate Perris <kateperrisuk@gmail.com>
2015-12-01 14:06:30 +0000

    Added color and price fields

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151201105409) do
+ActiveRecord::Schema.define(version: 20151201135449) do
 
   create_table "products", force: true do |t|
     t.string   "name"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 20151201105409) do
     t.string   "image_url"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "color"
+    t.integer  "price"
   end
 
 end
