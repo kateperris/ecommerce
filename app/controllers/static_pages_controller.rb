e769c04d91cb2edceae6ff58f13d0a5976e54a5d
Kate Perris <kateperrisuk@gmail.com>
2015-12-03 16:13:09 +0000

    Changes to links in application.html.erb

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,8 +1,9 @@
 class StaticPagesController < ApplicationController
   def index
+    @products = Product.limit(3)
   end
 
   def landing_page
-  	@featured_product = Product.first
+  	@products = Product.limit(3)
   end
 end
