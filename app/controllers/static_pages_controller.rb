6a4d2436f60b4f26b70b4ed73499f8414dae0fa2
Kate Perris <kateperrisuk@gmail.com>
2016-01-03 17:35:37 +0000

    Added email form and sendgrid

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -6,4 +6,15 @@ class StaticPagesController < ApplicationController
   def landing_page
   	@products = Product.limit(3)
   end
+
+  def thank_you
+    @name = params[:name]
+    @email = params[:email]
+    @message = params[:message]
+    ActionMailer::Base.mail(:from => @email,
+      :to => 'kateperrisuk@gmail.com',
+      :subject => "A new contact form message from #{@name}",
+      :body => @message).deliver
+  end
+
 end
