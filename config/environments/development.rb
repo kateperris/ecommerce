6d068d7c021c1248cb2335ab80adad96cf3f04f0
Kate Perris <kateperrisuk@gmail.com>
2016-01-09 16:51:29 +0000

    Added authentication and styled forms

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -18,6 +18,7 @@ Rails.application.configure do
 
   config.action_mailer.preview_path = "#{Rails.root}/test/mailers"
 
+  config.action_mailer.default_url_options = { :host => 'localhost:3000'}
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
