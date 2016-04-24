50f933d0c70d45832c222e38ea46d7cfd263f4f5
Kate Perris <kateperrisuk@gmail.com>
2016-04-24 13:30:09 +0100

    Removed duplicate line in routes

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,7 @@
 class ProductsController < ApplicationController
   respond_to :json, :html
   before_action :set_product, only: [:show, :edit, :update, :destroy]
-
+ 
   # GET /products
   # GET /products.json
   def index
--- config/routes.rb
+++ config/routes.rb
@@ -9,8 +9,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :users
-  
-  resources :products
+
 
 
   get 'static_pages/about'
